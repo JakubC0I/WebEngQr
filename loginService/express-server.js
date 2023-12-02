40d117aeae8cb8c2fe038f48f77278f080ad4bf3
Implomin <Implomin@gmail.com>
2023-12-02 16:49:14 +0100

    bake cookies
    small code clean-up
    added some cookies
    TODO add protected-route functionality and fix

--- loginService/express-server.js
+++ loginService/express-server.js
@@ -1,41 +1,53 @@
+//#region init
+//#region initial init?
+const path = require('path');
+//#endregion
+
+//#region express init
 const express = require('express');
 const portnumber = 4000;
 const server = express();
+server.use(express.static(path.join(__dirname, ".")));
+//#endregion 
 
+//#region pocketBase init
 const PocketBase = require('pocketbase/cjs')
 const pb = new PocketBase('http://127.0.0.1:8090')
 const authData = pb.admins.authWithPassword('admin@admin.admin', '"]SLD;,3d>rj~S2');
+//#endregion
 
-const path = require('path');
-const fs = require('fs');
-
+//#region parsers init
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json()
+//#endregion
 
-server.use(express.static(path.join(__dirname, ".")));
+//#region cookies init
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const secretKey = 'EeWhIW7NGEKIgUmzO0CWryDbwtJsx150'
+server.use(cookieParser());
+//#endregion
 
+//#region bcrypt init
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+//#endregion
+//#endregion
 
 //#region server operations
+//#region register
 server.post("/register", jsonParser, async function (req, res) {
 
     console.log("/register requested");
     const receivedLogin = req.body.login;
     const receivedPassword = req.body.password;
 
-    try
-    {
-        var test = await pb.collection('Accounts').getFirstListItem({ login_data: {
-            login: receivedLogin
-        }}).login;
-        console.log(test)
-        res.send("User with specified login already exists");        
-    }
-    catch
-    {
-        bcrypt.hash(receivedPassword, saltRounds, async function(err, hash) 
-        {
+    try {
+        const account = await pb.collection('Accounts').getFirstListItem(`login_data.login="${receivedLogin}"`)
+        res.send("User with specified login already exists");  
+        }
+    catch {
+        bcrypt.hash(receivedPassword, saltRounds, async function(err, hash) {
             await pb.collection('Accounts').create({ login_data: {
                 login: receivedLogin , password: hash
             }})
@@ -44,34 +56,59 @@ server.post("/register", jsonParser, async function (req, res) {
     }
     res.status(200);
 });
-
+//#endregion
+//#region login
 server.post("/login", jsonParser, async function (req, res) {
 
     console.log("/login requested");
     const receivedLogin = req.body.login;
     const receivedPassword = req.body.password;
 
-    var hashedPassFromDb = await pb.collection('Accounts').getFirstListItem('login='+receivedLogin, {
-        expand: 'relField1,relField2.subRelField',
-    });
+    const account = await pb.collection('Accounts').getFirstListItem(`login_data.login="${receivedLogin}"`)
+    //DUMB THING VERY DUMB WHY DO I HAVE TO ADD THOSE QUOTES WTHHH
 
-    var data;
-    try{
+    const hashedPassFromDb = account.login_data.password
+
+    try {
         var compare = await bcrypt.compare(receivedPassword, hashedPassFromDb)
-        if(compare)
-        {
-            data = "Correct credentials"
+        if(compare) {
+            const token = jwt.sign({ receivedLogin }, secretKey, { expiresIn: '1h' });
+            res.cookie('loginToken', token, { httpOnly: true, maxAge: 3600000 });
+            res.json({ success: true, message: 'Login successful' });
         }
-        else{
-            data = "Incorrect credentials; Password is wrong"
+        else {
+            res.json({ success: false, message: 'Wrong password' });
         }
     }catch(e){
         console.log(e)
-        data = "error"
+        res.json({ success: false, message: 'Error' });
     }
+});
+//#endregion
+//#region cookies/login protection
+server.get('/protected-route', (req, res) => {
+    const token = req.cookies.loginToken;
+  
+    if (!token) {
+      return res.status(401).json({ success: false, message: 'Unauthorized' });
+    }
+  
+    // Verify the token
+    jwt.verify(token, secretKey, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ success: false, message: 'Invalid token' });
+        // redirect to login page?
+      }
+  
+      // if the token is valid it will return the login upon decoding
+      const userLogin = decoded.userLogin;
+      
+      // redirect to protected path here
 
-    res.send(data);
-    console.log("sending response: " + data);
+      res.json({ success: true, message: `Welcome, user ${userLogin}!` });
+      res.send(userLogin)
+    });
+    res.status(200);
 });
 //#endregion
 
