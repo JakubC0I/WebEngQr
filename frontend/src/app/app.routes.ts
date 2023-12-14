bcde49fc7ce00a96fbf9700a6c8716c21121971b
jakubciszak <jakub.ciszak@tallence.com>
2023-12-14 15:44:25 +0100

    Add route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from "./form/form.component";
 import {ImageEditorComponent} from "./image-editor/image-editor.component";
 import {jwtGuard} from "./jwt.guard";
+import { IndexComponent } from './index/index.component';
 export const routes: Routes = [
   {path: "login", component: LoginComponent, data: {"action": "login"}},
   {path: "register", component: LoginComponent, data: {"action": "register"}},
-  {path: "ticket", component: ImageEditorComponent, canActivate: [jwtGuard]}
+  {path: "ticket", component: ImageEditorComponent, canActivate: [jwtGuard]},
+  {path : "", component: IndexComponent}
 ];
