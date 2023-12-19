4548e699ad352e1f64ce8e2b57c40d34c9c26cb1
Implomin <Implomin@gmail.com>
2023-12-19 10:31:48 +0100

    create draft for impressum
    draft generated by chatgpt

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { LoginComponent } from "./form/form.component";
 import {ImageEditorComponent} from "./image-editor/image-editor.component";
 import {jwtGuard} from "./jwt.guard";
 import { IndexComponent } from './index/index.component';
+import { ContactComponent } from './contact/contact.component';
 export const routes: Routes = [
   {path: "login", component: LoginComponent, data: {"action": "login"}},
   {path: "register", component: LoginComponent, data: {"action": "register"}},
   {path: "ticket", component: ImageEditorComponent, canActivate: [jwtGuard]},
+  {path: "contact", component: ContactComponent},
   {path : "", component: IndexComponent}
 ];
